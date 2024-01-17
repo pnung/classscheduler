b82d219f05a01eaa3e9f368b551ff02418b668af
ElliottF05 <elliotthfaa@gmail.com>
2024-01-17 17:49:03 -0500

    Add taskList to Task class

--- app/src/main/java/com/example/classscheduler/data/Task.java
+++ app/src/main/java/com/example/classscheduler/data/Task.java
@@ -1,12 +1,18 @@
 package com.example.classscheduler.data;
 
+import java.util.ArrayList;
+
 public class Task {
     private String name;
     private String description;
 
+    private static ArrayList<Task> taskList;
+
     public Task(String name, String description) {
         this.name = name;
         this.description = description;
+
+        taskList.add(this);
     }
 
     public Task(String name) {
