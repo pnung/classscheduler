82a17b8fffc07882ec60590bb287b5b47a0bfb48
Yuchan Cho <yuchan722cho@gmail.com>
2024-01-18 13:02:44 -0500

    added missing parameter and getter and setter for assignment class

--- app/src/main/java/com/example/classscheduler/data/Assignment.java
+++ app/src/main/java/com/example/classscheduler/data/Assignment.java
@@ -3,9 +3,19 @@ package com.example.classscheduler.data;
 public class Assignment extends Task {
     private String courseName;
 
-    public Assignment(String assignmentName, String courseName) {
+    private String dueDate;
+
+    public Assignment(String assignmentName, String dueDate, String courseName) {
         super(assignmentName);
+        this.dueDate = dueDate;
         this.courseName = courseName;
     }
 
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String newDueDate) {
+        dueDate = newDueDate;
+    }
 }
