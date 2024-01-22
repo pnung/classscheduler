7a9e092c02e7118ece35380c3307add78e7be07d
nungchucks <paunung04@gmail.com>
2024-01-21 21:15:07 -0500

    made course homepage and to do list independent

--- app/src/main/java/com/example/classscheduler/ui/todo/TodoFragment.java
+++ app/src/main/java/com/example/classscheduler/ui/todo/TodoFragment.java
@@ -4,129 +4,98 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.classscheduler.R;
 import com.example.classscheduler.data.Task;
-import com.example.classscheduler.databinding.FragmentTodoBinding;
-
+import com.example.classscheduler.ui.todo.TaskViewModel;
 import java.util.ArrayList;
 
 public class TodoFragment extends Fragment {
 
-    private FragmentTodoBinding binding;
+    private TaskViewModel taskViewModel;
+    private CustomAdapter taskArrayListAdapter;
 
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        TodoViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(TodoViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_todo, container, false);
 
-        binding = FragmentTodoBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        taskViewModel = new ViewModelProvider(this).get(TaskViewModel.class);
+
+        taskArrayListAdapter = new CustomAdapter(new ArrayList<>());
+        RecyclerView todoRecyclerView = root.findViewById(R.id.todo_recycler_view);
+        todoRecyclerView.setAdapter(taskArrayListAdapter);
+        todoRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        taskViewModel.getTasks().observe(getViewLifecycleOwner(), updatedList -> {
+            taskArrayListAdapter.updateData(updatedList);
+        });
 
         Button todoTaskAddButton = root.findViewById(R.id.todo_task_add_button);
-
-        // recycler view stuff
-
-        class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
-
-            private ArrayList<Task> localDataSet;
-
-            /**
-             * Provide a reference to the type of views that you are using
-             * (custom ViewHolder)
-             */
-            class ViewHolder extends RecyclerView.ViewHolder {
-                private final TextView taskName;
-                private final TextView taskDueDate;
-
-                public ViewHolder(View view) {
-                    super(view);
-                    // Define click listener for the ViewHolder's View
-
-                    taskName = (TextView) view.findViewById(R.id.taskName);
-                    taskDueDate = (TextView) view.findViewById(R.id.taskDueDate);
-                }
-
-                public TextView getTaskNameView() {
-                    return taskName;
-                }
-                public TextView getTaskDueDateView() {
-                    return taskDueDate;
-                }
-            }
-
-            /**
-             * Initialize the dataset of the Adapter
-             *
-             * @param dataSet String[] containing the data to populate views to be used
-             * by RecyclerView
-             */
-            public CustomAdapter(ArrayList<Task> dataSet) {
-                localDataSet = dataSet;
-            }
-
-            // Create new views (invoked by the layout manager)
-            @Override
-            public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-                // Create a new view, which defines the UI of the list item
-                View view = LayoutInflater.from(viewGroup.getContext())
-                        .inflate(R.layout.text_row_item, viewGroup, false);
-
-                return new ViewHolder(view);
-            }
-
-            // Replace the contents of a view (invoked by the layout manager)
-            @Override
-            public void onBindViewHolder(ViewHolder viewHolder, final int position) {
-
-                // Get element from your dataset at this position and replace the
-                // contents of the view with that element
-                viewHolder.getTaskNameView().setText(localDataSet.get(position).getName());
-                viewHolder.getTaskDueDateView().setText(localDataSet.get(position).getDescription());
-            }
-
-            // Return the size of your dataset (invoked by the layout manager)
-            @Override
-            public int getItemCount() {
-                return localDataSet.size();
-            }
-
-            public void add(Task task) {
-                localDataSet.add(task);
-            }
-        }
-
-
-        RecyclerView todoRecyclerView = root.findViewById(R.id.todo_recycler_view);
-        CustomAdapter taskArrayListAdapter = new CustomAdapter(Task.getTaskList());
-        todoRecyclerView.setAdapter(taskArrayListAdapter);
-        todoRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false));
-
-        todoTaskAddButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                System.out.println("hello");
-                Task T = new Task("taskName", "description");
-                todoRecyclerView.setAdapter(new CustomAdapter(Task.getTaskList()));
-            }
+        todoTaskAddButton.setOnClickListener(v -> {
+            Task newTask = new Task("Task Name", "Task Description");
+            // Add task to ViewModel
+            taskViewModel.addTask(newTask);
         });
 
         return root;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    private class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
+        private ArrayList<Task> localDataSet;
+
+        CustomAdapter(ArrayList<Task> dataSet) {
+            localDataSet = dataSet;
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+            View view = LayoutInflater.from(viewGroup.getContext())
+                    .inflate(R.layout.todo_list_item, viewGroup, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+            Task task = localDataSet.get(position);
+            viewHolder.getTaskNameView().setText(task.getName());
+            viewHolder.getTaskDescriptionView().setText(task.getDescription());
+        }
+
+        @Override
+        public int getItemCount() {
+            return localDataSet.size();
+        }
+
+        public void updateData(ArrayList<Task> newData) {
+            localDataSet = newData;
+            notifyDataSetChanged();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            private final TextView taskNameView;
+            private final TextView taskDescriptionView;
+
+            ViewHolder(View view) {
+                super(view);
+                taskNameView = view.findViewById(R.id.taskName);
+                taskDescriptionView = view.findViewById(R.id.taskDueDate);
+            }
+
+            TextView getTaskNameView() {
+                return taskNameView;
+            }
+
+            TextView getTaskDescriptionView() {
+                return taskDescriptionView;
+            }
+        }
     }
 }
