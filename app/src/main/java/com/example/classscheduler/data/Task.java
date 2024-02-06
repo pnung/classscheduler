f359bd6b9cb8d55e5c0b59fe6aa839e0027d7b02
ElliottF05 <elliotthfaa@gmail.com>
2024-02-06 13:35:16 -0500

    add course sorting

--- app/src/main/java/com/example/classscheduler/data/Task.java
+++ app/src/main/java/com/example/classscheduler/data/Task.java
@@ -6,16 +6,18 @@ public class Task {
     private String name;
     private String description;
     private DateAndTime primaryDateAndTime;
+    private String course;
 
-    public Task(String name, String description, DateAndTime dateAndTime) {
+    public Task(String name, String description, DateAndTime dateAndTime, String course) {
         System.out.println("creating task");
         this.name = name;
         this.description = description;
         this.primaryDateAndTime = dateAndTime;
+        this.course = course;
     }
 
     public Task(String name) {
-        this(name, "", new DateAndTime());
+        this(name, "", new DateAndTime(), "");
     }
 
     @Override
@@ -49,11 +51,31 @@ public class Task {
         return primaryDateAndTime;
     }
 
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
     public static class AlphabeticalSort implements Comparator<Task> {
 
         @Override
         public int compare(Task o1, Task o2) {
             return o1.name.toLowerCase().compareTo(o2.name.toLowerCase());
+        }
+    }
+
+    public static class CourseSort implements Comparator<Task> {
+        @Override
+        public int compare(Task o1, Task o2) {
+            int compareResult = o1.course.toLowerCase().compareTo(o2.course.toLowerCase());
+            ChronologicalSort backupSort = new ChronologicalSort();
+            if (compareResult == 0) {
+                return backupSort.compare(o1, o2);
+            }
+            return compareResult;
         }
     }
 
@@ -72,12 +94,12 @@ public class Task {
                 return o1.primaryDateAndTime.getDay() - o2.primaryDateAndTime.getDay();
             } else if (o1.primaryDateAndTime.isPM() != o2.primaryDateAndTime.isPM()) {
                 if (o1.primaryDateAndTime.isPM()) {
-                    return -1;
-                } else {
                     return 1;
+                } else {
+                    return -1;
                 }
             } else if (o1.primaryDateAndTime.getHour() != o2.primaryDateAndTime.getHour()) {
-                return (o1.primaryDateAndTime.getHour() % 12)- (o2.primaryDateAndTime.getHour() % 12);
+                return (o1.primaryDateAndTime.getHour() % 12) - (o2.primaryDateAndTime.getHour() % 12);
             } else {
                 return o1.primaryDateAndTime.getMinute() - o2.primaryDateAndTime.getMinute();
             }
